772ecf961ceb136cbd0fbb5abe97bd3c10c115f3
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-31 09:50:22 +0200

    code fixed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -66,40 +66,40 @@ export default {
 <template lang="">
     <div class="container-fluid px-0">
         <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="light">
-        <div class="container-fluid">
-            <div>
-                <a class="navbar-brand" href="#">Deliveboo</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <div class="container-fluid">
+                <div>
+                    <a class="navbar-brand" href="#">Deliveboo</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="right-search d-flex">
+                    <AppSearch @search="searchAll"/>    
+                </div>
+                <select name="" id="" v-model="type" @change="filter">
+                    <option :value="type.name" v-for="(type) in types">
+                        {{ type.name }}
+                    </option>
+                </select>
+
+                <div class="collapse navbar-collapse flex-row-reverse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item" v-for="(item, index) in menuItems" :key="index">
+                            <!--<a :href="item.routeName" class="nav-link">
+                                {{ item.label }}
+                            </a> -->
+                            <router-link :to="{ name: item.routeName }" class="nav-link">
+                                {{ item.label }}
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link :to="{ name: 'CartPage' }" class="nav-link">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="right-search d-flex">
-                <AppSearch @search="searchAll"/>
-                
-            </div>
-            <select name="" id="" v-model="type" @change="filter">
-                <option :value="type.name" v-for="(type) in types">
-                    {{ type.name }}
-                </option>
-            </select>
-            <div class="collapse navbar-collapse flex-row-reverse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item" v-for="(item, index) in menuItems" :key="index">
-                    <!--<a :href="item.routeName" class="nav-link">
-                        {{ item.label }}
-                    </a> -->
-                    <router-link :to="{ name: item.routeName }" class="nav-link">
-                        {{ item.label }}
-                    </router-link>
-                </li>
-                <li>
-                    <router-link :to="{ name: 'CartPage' }" class="nav-link">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                    </router-link>
-                </li>
-            </ul>
-            </div>
-        </div>
         </nav>
     </div>
 </template>
